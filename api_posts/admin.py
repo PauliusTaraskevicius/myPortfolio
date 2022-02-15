1006d985d15e34af835730b6ce85bfe76be6e38a
PauliusTaraskevicius <fixas1029@gmail.com>
2022-02-15 09:02:30 +0200

    Post model

--- api_posts/admin.py
+++ api_posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+@admin.register(Post)
+class PostModel(admin.ModelAdmin):
+    list_filter = ('title', 'sub_title', 'created')
+    list_display = ('title', 'sub_title', 'created')
+    prepopulated_fields = {'slug': ('title',)}
--- api_posts/models.py
+++ api_posts/models.py
@@ -2,3 +2,19 @@ from django.db import models
 from slugify import slugify
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    sub_title = models.CharField(max_length=100, blank=True)
+    thumbnail = models.ImageField(null=True, upload_to='images', default='default.jpg')
+    body = models.TextField(max_length=1000, null=True, blank=False)
+    technologies_used = models.TextField(max_length=1000, null=True, blank=False)
+    url = models.URLField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.headline)
+        super(Post, self).save(*args, **kwargs)
