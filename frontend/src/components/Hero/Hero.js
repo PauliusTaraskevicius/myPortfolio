c75d5f5c32e82febbe1645aacb2bd07d7123605a
PauliusTaraskevicius <fixas1029@gmail.com>
2022-06-02 08:19:50 +0300

    added favicon, terminal bubbles

--- frontend/src/components/Hero/Hero.js
+++ frontend/src/components/Hero/Hero.js
@@ -16,9 +16,17 @@ const Hero = () => {
     >
       <section className="w-full h-screen">
         <div className="container w-full h-full px-8 sm:px-0 mx-auto flex flex-col justify-center">
-          <h1 className="line anim-typewriter herotxt text-3xl font-bold mb-2">
-            Hey, I'm Paulius!
-          </h1>
+          <div className="flex">
+            <div className="h-3 w-3 bg-red-500 rounded-full"></div>
+            <div className="ml-2 h-3 w-3 bg-orange-300 rounded-full"></div>
+            <div className="ml-2 h-3 w-3 bg-green-500 rounded-full"></div>
+          </div>
+          <div className="mt-4 flex">
+            <h1 className="line anim-typewriter herotxt text-3xl font-bold mb-2">
+              Hey, I'm Paulius!
+            </h1>
+          </div>
+
           <p className="text-white break-words mt-2 lg:w-1/2">
             Web developer from Kaunas, Lithuania. Creating web applications as
             my hobby and full time job.I create custom websites, complex systems
